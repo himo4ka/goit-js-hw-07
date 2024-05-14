86245f720193a9028beb292f9616d0124513d472
Marina Baronina <baronina.mi@gmail.com>
2024-05-14 18:35:54 +0300

    added trimmed values in the 4th task

--- js/task-4.js
+++ js/task-4.js
@@ -1,11 +1,18 @@
 const form = document.querySelector(".login-form");
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    
     const { elements: { email, password } } = event.currentTarget;
-    if (email.value === "" || password.value === "") {
+    
+    const trimmedEmail = email.value.trim();
+    const trimmedPassword = password.value.trim();
+    
+    if (trimmedEmail === "" || trimmedPassword === "") {
         alert("All form fields must be filled in");
     } else {
-        console.log({ "email": email.value, "password": password.value });
+        console.log({ "email": trimmedEmail, "password": trimmedPassword });
         form.reset();
     }
 });
+
